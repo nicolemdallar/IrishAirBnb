4d4836a2310ccf5e2f66f14be45680d83fc2002e
Nicole Dallar <57956592+nicolemdallar@users.noreply.github.com>
2020-03-24 21:04:58 -0500

    Create function iqrRemoveOutliers
    Removes the outliers in numeric columns based on the interquartile rule

--- Irish_functions.R
+++ Irish_functions.R
@@ -9,3 +9,12 @@ formatMoney = function(x) {
   new_price = as.numeric(no_comma)
   return(new_price)
 }
+
+iqrRemoveOutliers = function(x) {
+  iqr = IQR(x, na.rm = T)
+  firstQ = quantile(x, na.rm = T)[2]
+  thirdQ = quantile(x, na.rm = T)[4]
+  minRemove = firstQ - (1.5 * iqr)
+  maxRemove = thirdQ + (1.5 * iqr)
+  return(paste0("For this column remove any values below ", minRemove, " or above ", maxRemove, "."))
+}
